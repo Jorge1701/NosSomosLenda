68d81f6e597c0fcb0b4e4a3e1a556d6889f3cb3a
AndresHein <andreshein29@gmail.com>
2018-10-17 23:47:38 -0300

    'cambiosanormales'

---
+++ Assets/Skybox/linternaka.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var encendida : boolean;
+public var linterna : Light;
+
+function Start () {
+    
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.Space)){
+	encendida = !encendida;
+		if (encendida){
+			linterna.enabled = false;
+		}
+		
+		else{
+			linterna.enabled = true;
+		}
+	}	
+}
