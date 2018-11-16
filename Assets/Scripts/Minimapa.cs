823af3c24d894c3cc1e2ee64b5e9bd03c53fe369
Luis Etchebarne <etchebarneluis@gmail.com>
2018-11-16 20:55:38 -0300

    barra de vida, minimapa y progreso de la cura

---
+++ Assets/Scripts/Minimapa.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimapa : MonoBehaviour {
+
+    public Transform jugador;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 nuevaPosicion = jugador.position;
+        nuevaPosicion.y = transform.position.y;
+        transform.position = nuevaPosicion;
+	}
+}
